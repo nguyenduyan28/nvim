c92927fa0440b39ca15c457ff634d2c2c9a6865e
nguyenduyan28 <nguyenduyan28@gmail.com>
2026-06-09 17:07:43 +0700

    update stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,8 +9,12 @@ map({ "n", "i", "v" }, "<D-p>", "<cmd>Telescope find_files<cr>", { desc = "Find 
 -- Command palette like VSCode Cmd+Shift+P
 map({ "n", "i", "v" }, "<D-S-p>", "<cmd>Telescope commands<cr>", { desc = "Command palette" })
 
--- Search text in project
+-- Search text in project like VSCode Cmd+Shift+F (works in normal, insert, visual)
+map({ "n", "i", "v" }, "<D-S-f>", "<cmd>Telescope live_grep<cr>", { desc = "Search in project" })
 map("n", "<leader>f", "<cmd>Telescope live_grep<cr>", { desc = "Search in project" })
+
+-- Search text in current file like VSCode Cmd+F (works in normal, insert, visual)
+map({ "n", "i", "v" }, "<D-f>", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Search in file" })
 
 -- Go to Symbol in file like VSCode Cmd+Shift+O
 map({ "n", "i", "v" }, "<D-S-o>", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Symbols in file" })
@@ -36,12 +40,44 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 -- Git panel
 map("n", "<leader>g", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
 
+-- Is the Diffview tab currently open? (without force-loading the plugin)
+local function diffview_is_open()
+  if not package.loaded["diffview"] then
+    return false
+  end
+  return require("diffview.lib").get_current_view() ~= nil
+end
+
+-- VSCode-like Source Control: toggle changed-files list + side-by-side diff (Cmd+Shift+G)
+map({ "n", "i", "v" }, "<D-S-g>", function()
+  if diffview_is_open() then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen")
+  end
+end, { desc = "Toggle source control (diff view)" })
+map("n", "<leader>gq", "<cmd>DiffviewClose<cr>", { desc = "Close diff view" })
+map("n", "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File history (current file)" })
+map("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", { desc = "File history (project)" })
+
 -- Go back / forward like VSCode
 map("n", "<A-Left>", "<C-o>", { desc = "Go back" })
 map("n", "<A-Right>", "<C-i>", { desc = "Go forward" })
 
 -- Close buffer
 map("n", "<leader>w", "<cmd>bdelete<cr>", { desc = "Close buffer" })
+
+-- Close current tab/view like VSCode Cmd+W:
+-- Diffview tab -> close it cleanly; multiple tabs -> close tab; otherwise close buffer
+map({ "n", "i", "v" }, "<D-w>", function()
+  if diffview_is_open() then
+    vim.cmd("DiffviewClose")
+  elseif #vim.api.nvim_list_tabpages() > 1 then
+    vim.cmd("tabclose")
+  else
+    vim.cmd("bdelete")
+  end
+end, { desc = "Close tab / view / buffer" })
 
 -- Clear search highlight
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search" })
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,4 +12,23 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+
+  -- VSCode-like Source Control: file list + side-by-side diff + staging
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFileHistory" },
+    config = function()
+      require("diffview").setup({
+        enhanced_diff_hl = true,
+        view = {
+          -- side-by-side (2 split) layout like VSCode
+          default = { layout = "diff2_horizontal" },
+          merge_tool = { layout = "diff3_horizontal" },
+        },
+      })
+    end,
+  },
 }
